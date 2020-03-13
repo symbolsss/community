7e2ab1a4d26adc8e8c24560cae25b1686186b864
symbolsss <321959601@qq.com>
2020-03-13 11:17:36 +0800

    add index and bootstrap

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller                   //自动识别扫描当前的类，把它当成spring的并举管理，同时识别它为一个controller。
                               // controller：允许这个类接收前端的请求
-public class HelloController {
+/**
+public class IndexController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model){   //RequestParam:请求参数.Ctrl+P：快捷键，提示应该传扫描参数
         model.addAttribute("name",name);                                       // 浏览器中的值，存放到Model中。
-        return "hello";                                                           //自动去模板目录“templates”寻早“hello”模板。
+        return "index";                                                           //自动去模板目录“templates”寻早“index”模板。
     }
 }
+*/
+public class IndexController {
+    @GetMapping("/")  //一个反斜杠，代表根；
+    public String index(){
+        return "index";
+    }
+}
+
